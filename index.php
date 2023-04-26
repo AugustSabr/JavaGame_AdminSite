d7360359aa1435edde434edd69c3e27430fc66fb
AugustSabr <augustsabr@gmail.com>
2023-04-26 13:00:42 +0200

    x

--- index.php
+++ index.php
@@ -10,13 +10,6 @@
   <title>Homepage</title>
 </head>
 <body id="ibody">
-<?php
-    session_start();
-    if (isset($_SESSION['privileges'])) {
-      unset($_SESSION['privileges']);
-    }
-    include './php/connect.php';
-  ?>
 <span id="modalButton" class="loginORout">Logg inn</span>
 
 <!-- The Modal -->
