af5a113e677bdf37581acf4534d7e35c412bb198
AugustSabr <augustsabr@gmail.com>
2023-04-28 10:39:49 +0200

    t

--- php/connect.php
+++ php/connect.php
@@ -5,6 +5,7 @@ $database = "game4";
 $username = "postgres";
 $password = "123";
 
+echo "host=$IP port=$Port dbname=$database user=$username password=$password"
 $conn = pg_connect("host=$IP port=$Port dbname=$database user=$username password=$password");
 
 pg_query($dbconn, "select * from doesnotexist");
