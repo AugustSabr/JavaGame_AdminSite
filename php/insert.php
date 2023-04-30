cc4410da57cfeb093867ce4cbf0f565a6bf908c4
Augustsabr <augustsabr@gmail.com>
2023-04-30 13:02:55 +0200

    sql syntax: "" er for å beholde upper case

--- php/insert.php
+++ php/insert.php
@@ -17,19 +17,19 @@
       $sql = 'INSERT INTO "gameTables".'.$dbtable.'s ("'.$dbtable.'Type") VALUES ('."'".$type."'".');';
       if($tierExists != 0){
         $tier =  pg_escape_string($conn, $_POST[$dbtable.'Tier']);
-        $sql .= 'UPDATE "gameTables".'.$dbtable.'s SET '.$dbtable.'Tier = '.$tier.' WHERE '.$dbtable.'Type='.$type.';';
+        $sql .= 'UPDATE "gameTables".'.$dbtable.'s SET '.$dbtable.'Tier = '.$tier.' WHERE "'.$dbtable.'Type"='.$type.';';
       }
       if($effectExists != 0){
         $effect =  pg_escape_string($conn, $_POST[$dbtable.'Effect']);
-        $sql .= 'UPDATE "gameTables".'.$dbtable.'s SET '.$dbtable.'Effect = '.$effect.' WHERE '.$dbtable.'Type='.$type.';';
+        $sql .= 'UPDATE "gameTables".'.$dbtable.'s SET '.$dbtable.'Effect = '.$effect.' WHERE "'.$dbtable.'Type"='.$type.';';
       }
       if($damageExists != 0){
         $damage =  pg_escape_string($conn, $_POST[$dbtable.'Damage']);
-        $sql .= 'UPDATE "gameTables".'.$dbtable.'s SET '.$dbtable.'Damage = '.$damage.' WHERE '.$dbtable.'Type='.$type.';';
+        $sql .= 'UPDATE "gameTables".'.$dbtable.'s SET '.$dbtable.'Damage = '.$damage.' WHERE "'.$dbtable.'Type"='.$type.';';
       }
       if($healthExists != 0){
         $health =  pg_escape_string($conn, $_POST[$dbtable.'Health']);
-        $sql .= 'UPDATE "gameTables".'.$dbtable.'s SET '.$dbtable.'Health = '.$health.' WHERE '.$dbtable.'Type='.$type.';';
+        $sql .= 'UPDATE "gameTables".'.$dbtable.'s SET '.$dbtable.'Health = '.$health.' WHERE "'.$dbtable.'Type"='.$type.';';
       }
     } else if($dbtable == 'user'){
       $location = "Location: ../manageProfiles.php";
